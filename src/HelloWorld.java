e085d2e450fc152e788e3313535961345120461f
FAN <465989260@qq.com>
2025-11-01 17:22:10 +0800

    学习java

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!\n");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+            //学习使用git
+            //学习使用分支
+        }
+    }
+}
